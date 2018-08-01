69cb878929c2b8e73c6a846185e6115e49be1f28
armink <armink.ztl@gmail.com>
2018-08-01 17:17:37 +0800

    【更新】软件版本号。
    Signed-off-by: armink <armink.ztl@gmail.com>

--- easyflash/inc/easyflash.h
+++ easyflash/inc/easyflash.h
@@ -76,7 +76,8 @@ if (!(EXPR))                                                                  \
 #endif
 
 /* EasyFlash software version number */
-#define EF_SW_VERSION                  "3.1.0"
+#define EF_SW_VERSION                  "3.2.0"
+#define EF_SW_VERSION_NUM              0x30200
 
 typedef struct _ef_env {
     char *key;
